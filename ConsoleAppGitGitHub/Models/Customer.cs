d190aefb3eeea457b60b29846fd35ae5b7953692
pcrantz <patrick.crantz@marquette.edu>
2023-11-28 13:05:52 -0600

    Created the Model Folder and the customer class

---
+++ ConsoleAppGitGitHub/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitGitHub.Models
+{
+    internal class Customer
+    {
+    }
+}
